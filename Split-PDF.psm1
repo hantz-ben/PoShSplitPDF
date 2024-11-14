7d938805e2429a97e5bc86dfc434c022d5754c67
hantz-ben <76164237+hantz-ben@users.noreply.github.com>
2024-11-14 08:20:50 -0500

    Add files via upload
    Module file with Split-PDF function

---
+++ Split-PDF.psm1
@@ -0,0 +1,38 @@
+﻿######################################
+## Assembly & Config
+######################################
+## Load needed assemblies for iText PDF
+[System.Reflection.Assembly]::LoadFrom('.\Resources\BouncyCastle.Crypto.dll') | Out-Null
+[System.Reflection.Assembly]::LoadFrom('.\Resources\Common.Logging.Core.dll') | Out-Null
+[System.Reflection.Assembly]::LoadFrom('.\Resources\Common.Logging.dll') | Out-Null
+[System.Reflection.Assembly]::LoadFrom('.\Resources\itext.io.dll') | Out-Null
+[System.Reflection.Assembly]::LoadFrom('.\Resources\itext.kernel.dll') | Out-Null
+
+####################################
+## Functions
+####################################
+Function Split-PDF {
+    <#
+        .SYNOPSIS
+            Create a new PDF file from pages in Source PDF using iText assemblies.
+
+        .EXAMPLE
+            Split-PDF -Source 'C:\Temp\Source.pdf' -Output 'C:\Temp\NewFile.pdf' -StartPg 4 -EndPg 8
+    #>
+    [CmdletBinding()]
+    Param (
+        [String]$Source, 
+        [String]$Output,
+        [Int]$StartPg,
+        [Int]$EndPg        
+    )
+
+    $Output = $Output.Replace('"','')
+    $SourcePDF = [iText.Kernel.Pdf.PdfDocument]::new([iText.Kernel.Pdf.PdfReader]::new($Source))
+    $NewPDF = [iText.Kernel.Pdf.PdfDocument]::new([iText.Kernel.Pdf.PdfWriter]::new($Output))
+
+    $SourcePDF.CopyPagesTo($StartPg, $EndPg, $NewPDF, 1) | Out-Null
+    
+    $NewPDF.Close()
+    $SourcePDF.Close()
+}
